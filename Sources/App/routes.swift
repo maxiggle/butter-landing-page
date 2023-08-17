eec6c0d1edaba707a4df94dadd62b7bed3769717
maxiggle <clsyfriday@gmail.com>
2023-08-17 13:07:29 +0100

    added text and card

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,4 +3,5 @@ import Vapor
 func routes(_ app: Application) throws {
      let websiteController = WebsiteController()
      try app.register(collection: websiteController)
+    let apiConroller = ApiController()
 }
