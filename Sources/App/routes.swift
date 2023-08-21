2f90facc3751d77f615103ee500ebe6929b781ef
maxiggle <clsyfriday@gmail.com>
2023-08-21 20:23:31 +0100

    removed apiController from routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,5 +3,4 @@ import Vapor
 func routes(_ app: Application) throws {
      let websiteController = WebsiteController()
      try app.register(collection: websiteController)
-    let apiConroller = ApiController()
 }
